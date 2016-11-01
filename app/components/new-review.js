df536dc2392d416c5c71fbe14a88282935aa1682
elysia nason <elysia.avery@gmail.com>
2016-10-31 22:18:55 -0700

    add hw

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -7,13 +7,14 @@ export default Ember.Component.extend({
       this.set('addNewReview', true);
     },
     saveReview() {
-      var params = {
-        author: this.get('author'),
-        rating: this.get('rating'),
-        content: this.get('content')
-      };
-      this.set('addNewReview', false);
-      this.sendAction('saveReview', params);
-    }
+     var params = {
+       author: this.get('author'),
+       rating: parseInt(this.get('rating')),
+       content: this.get('content'),
+       rental: this.get('rental')
+     };
+     this.set('addNewReview', false);
+     this.sendAction('saveReview', params);
+   }
   }
 });
